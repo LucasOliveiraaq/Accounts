05368d08b04d1c5c7bb1ac68aa2b2bcc7194c184
Lucas P Oliveira <lucasp.oliveira122@gmail.com>
2023-09-09 14:13:54 -0300

    Sacar

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function operation() {
         } else if(action === 'Depositar'){
             deposit();
         } else if(action === 'Sacar') {
-
+            widthdraw();
         } else if(action === 'Sair') { 
             console.log(chalk.bgBlue.black('Obrigado por usar o Accounts!'));
             process.exit();
@@ -154,4 +154,52 @@ function getAccountBalance(){
         operation();
     })
     .catch((err) => console.log(err));
+}
+
+// widthdraw am amount from user account
+function widthdraw() {
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'Qual o nome da sua conta?'
+        }
+    ])
+    .then((answer) => {
+        const accountName = answer['accountName'];
+        if(!checkAccount(accountName)){
+            return widthdraw();
+        }
+        inquirer.prompt([
+            {
+                name: 'amount',
+                message: 'Quanto você deseja sacar?'
+            }
+        ])
+        .then((answer) => {
+            const amount = answer['amount'];
+            removeAmount(accountName, amount);
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
+}
+
+function removeAmount(accountName, amount){
+    const accountData = getAccount(accountName);
+    if(!amount){
+        console.log(chalk.bgRed.black('Ocorreu um erro, tente novamente mais tarde!'));
+        return widthdraw();
+    }
+    if(accountData.balance < amount){
+        console.log(chalk.bgRed.black('Valor indisponível!'));
+        return widthdraw();
+    }
+
+    accountData.balance = parseFloat(accountData.balance) - parseFloat(amount);
+    fs.writeFileSync('accounts/' + accountName + '.json', JSON.stringify(accountData), function(err){
+        console.log(err);
+    })
+
+    console.log(chalk.green('Foi realizado um saque de R$ ' + amount + ' da sua conta!'));
+    operation();
 }
